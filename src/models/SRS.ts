4ebb78b41e895954ab4553d1849f66ab81a39810
Ekansh <zethyst@protonmail.com>
2024-07-13 23:38:26 +0530

    Initial Commit

---
+++ src/models/SRS.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface SRS extends Document {
+  owner: mongoose.Types.ObjectId;
+  name: string;
+  description: string[];
+  status: string;
+  pdf_url: string;
+  word_url: string;
+  createdAt: Date;
+}
+
+const SRSSchema: Schema<SRS> = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // this should be "User" to reference the User model
+    required: [true, "owner is required"],
+  },
+  name: {
+    type: String,
+  },
+  description: {
+    type: [String],
+  },
+  status: {
+    type: String,
+    required: true,
+    default:"Generating"
+  },
+  pdf_url: {
+    type: String,
+  },
+  word_url: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const SRSModel =
+  (mongoose.models.SRS as mongoose.Model<SRS>) ||
+  mongoose.model<SRS>("SRS", SRSSchema);
+
+export default SRSModel;
