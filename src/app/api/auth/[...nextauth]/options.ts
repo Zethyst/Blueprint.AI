7fc31dc833c309acaed1629851de8cb1ff8f94b1
Ekansh <zethyst@protonmail.com>
2025-11-27 04:01:53 +0530

    improved login provider icons

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,6 +7,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
 import AppleProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -21,6 +22,10 @@ export const authOptions: NextAuthOptions = {
       TwitterProvider({
         clientId: process.env.TWITTER_CLIENT_ID as string,
         clientSecret: process.env.TWITTER_CLIENT_SECRET as string
+      }),
+      FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID as string,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
       }),
       AppleProvider({
         clientId: process.env.APPLE_ID as string,
